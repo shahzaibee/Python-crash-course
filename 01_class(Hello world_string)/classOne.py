7a9e3216a817a1f939e3fb0abe1e28be001f7889
Shahzaib <shahzaib1224@hotmail.com>
2023-11-10 19:16:37 +0500

    class one code revise

--- 01_class(Hello world_string)/classOne.py
+++ 01_class(Hello world_string)/classOne.py
@@ -1,6 +1,14 @@
 print("hello world")
 print("Hello world")
 
+from typing import Any
+
+name :Any = "Pakistan"
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physcial address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
 
 
 
